9177abf8eacf0c650ffa0751e6330d3b2dc4e44f
Wilermo <william.dmo.sec@gmail.com>
2023-04-21 11:55:48 -0500

    Maps 1

--- app/src/main/java/com/example/taller2compumovil/MapaActivity.java
+++ app/src/main/java/com/example/taller2compumovil/MapaActivity.java
@@ -1,5 +1,83 @@
 package com.example.taller2compumovil;
 
-public class MapaActivity {
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.example.taller2compumovil.databinding.MapaActivityBinding;
+import com.example.taller2compumovil.fragments.MapsFragment;
+import com.example.taller2compumovil.services.LocationService;
+import com.example.taller2compumovil.services.PermissionService;
+import com.google.android.gms.location.LocationAvailability;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationResult;
+
+
+import javax.inject.Inject;
+
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
+public class MapaActivity extends MainActivity
+{
+    public static final String TAG = MainActivity.class.getName();
+
+    MapaActivityBinding binding;
+
+    @Inject
+    PermissionService permissionService;
+
+    @Inject
+    LocationService locationService;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = MapaActivityBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        getSupportActionBar().hide();
+
+        locationService.setLocationCallback(new LocationCallback() {
+            @Override
+            public void onLocationAvailability(@NonNull LocationAvailability locationAvailability) {
+                super.onLocationAvailability(locationAvailability);
+            }
+
+            @Override
+            public void onLocationResult(@NonNull LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                Log.i(TAG, "onLocationResult: " + locationResult.getLastLocation().toString());
+                MapsFragment fragment = binding.map.getFragment();
+                fragment.updateUserPositionOnMap(locationResult);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        permissionService.getLocationPermission(this);
+        if (permissionService.isMLocationPermissionGranted()) {
+            locationService.startLocation();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        locationService.stopLocation();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PermissionService.PERMISSIONS_REQUEST_LOCATION) {
+            permissionService.getLocationPermission(this);
+            if (permissionService.isMLocationPermissionGranted()) {
+                locationService.startLocation();
+            }
+        }
+    }
 
 }
---
+++ app/src/main/java/com/example/taller2compumovil/services/GeoInfoFromJsonService.java
@@ -0,0 +1,59 @@
+package com.example.taller2compumovil.services;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.example.taller2compumovil.model.GeoInfo;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import dagger.Module;
+import dagger.hilt.InstallIn;
+import dagger.hilt.android.components.ActivityComponent;
+import dagger.hilt.android.qualifiers.ApplicationContext;
+import lombok.Getter;
+
+@Getter
+@Module
+@InstallIn(ActivityComponent.class)
+public class GeoInfoFromJsonService {
+    public static final String TAG = GeoInfoFromJsonService.class.getName();
+    public static final String COUNTRIES_FILE = "nyc_info.json";
+    private final Context context;
+    private ArrayList<GeoInfo> geoInfoList = new ArrayList<>();
+
+    @Inject
+    public GeoInfoFromJsonService(@ApplicationContext Context context) {
+        this.context = context;
+        loadGeoInfoFromJson();
+    }
+
+    public String loadJSONFromAsset() {
+        String json = null;
+        try {
+            InputStream is = context.getAssets().open(COUNTRIES_FILE);
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, StandardCharsets.UTF_8);
+        } catch (IOException ex) {
+            Log.e(TAG, String.format("loadJSONFromAsset: error reading the %s file.", COUNTRIES_FILE), ex);
+            return null;
+        }
+        return json;
+    }
+
+    public void loadGeoInfoFromJson() {
+        geoInfoList = new Gson().fromJson(loadJSONFromAsset(), new TypeToken<List<GeoInfo>>() {}.getType());
+        Log.d(TAG, String.format("loadGeoInfoFromJson: loaded %d registries.", geoInfoList.size()));
+    }
+}
